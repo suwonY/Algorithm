e7032a737d13362920bb27984533a2b16a5f9dec
surisuri <dongpeeee@gmail.com>
2017-11-29 01:08:39 +0900

    17.11.29

--- java_source/Baek1759.java
+++ java_source/Baek1759.java
@@ -1,30 +1,27 @@
-package pracAlgorithm;
+package week_15th_thu;
 
 import java.util.*;
 public class Baek1759 {
-	static char[] a;
-	static int L, C;
-	static void go(int index, int cnt, int mo, int ja, String s){
-		if(cnt==L){
-			if(mo>=1 && ja>=2) System.out.println(s);
+
+	static int n,m;
+	static char[] c = new char[16];
+	public static void go(int cnt, String now, int i){
+		if(now.length()>cnt) return;
+		if(cnt==4){
+			System.out.println(now);
 			return;
 		}
-		if(index==C) return;
-		if (a[index] == 'a' || a[index] == 'e' || a[index] == 'i' || a[index] == 'o' || a[index] == 'u')
-			go(index + 1, cnt + 1, mo + 1, ja, s + a[index]);
-		else
-			go(index + 1, cnt + 1, mo, ja + 1, s + a[index]);
-		go(index+1, cnt, mo, ja, s);
+		
 	}
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		L = in.nextInt();
-		C = in.nextInt();
-		a = new char[C];
-		for(int i=0; i<C; i++)
-			a[i] = in.next().charAt(0);
-		Arrays.sort(a);
-		go(0,0,0,0,"");
+		n = in.nextInt();
+		m = in.nextInt();
+		for(int i=0; i<m; i++)
+			c[i] = in.next().charAt(0);
+		
+		
 		in.close();
 	}
+
 }
--- java_source/Baek2931.java
+++ java_source/Baek2931.java
@@ -1,16 +1,31 @@
-package pracAlgorithm;
+package week_12th_thu;
 
 import java.util.*;
 public class Baek2931 {
+	/*가스관
+	 * 블럭의 종류는 
+	 * | 상하 연결
+	 * - 좌우 연결
+	 * + 상하좌우 연결
+	 * 1 밑 - 오른쪽 연결	┌
+	 * 2 위 - 오른쪽 연결 	└
+	 * 3 왼쪽 - 위 연결 	┘
+	 * 4 왼쪽 - 아래 연결	┐
+	 * 
+	 * M은 모스크바
+	 * Z 자그레브
+	 * 
+	 * 블럭을 하나를 채워서 가스의 흐름이 유일하게 만들기
+	 * */
 	static int n,m;
 	static int[] sdx = {-2,2,0,0,1,1,-1,-1};
 	static int[] sdy = {0,0,2,-2,1,-1,1,-1};
 	static int[] dx = {-1,1,0,0}, dy = {0,0,-1,1};
 	static String[] p = {"1111","1100","0011","0101","1001","1010","0110"};
-	static char[][] a = new char[26][26];				
+	static char[][] a = new char[26][26];
 	static int[][] d = new int[26][26];
 	static boolean[][] c = new boolean[26][26];
-	static int sx,sy,ex,ey,ax,ay;
+	static int sx,sy,ax,ay;
 	public static int pipe(char c){
 		if(c=='+') return 0;
 		else if(c=='|') return 1;
@@ -40,6 +55,7 @@ public class Baek2931 {
 		int dnx = nx - x;
 		int dny = ny - y;
 		int dir = 0;
+		//남의놈이 상하좌우 중 어디에 있는 놈인지 확인하기
 		for(int i=0; i<4; i++){
 			if(dnx==dx[i] && dny==dy[i]){
 				dir=opposite(i);
@@ -50,27 +66,17 @@ public class Baek2931 {
 			return 1;
 		else return 0;
 	}
-	public static void go(){
+	public static void go(Point start){
 		Queue<Point> q = new LinkedList<>();
+
 		for(int i=0; i<4; i++){
-			int nx = sx + dx[i];
-			int ny = sy + dy[i];
+			int nx = start.x + dx[i];
+			int ny = start.y + dy[i];
 			if(nx<0 || nx>=n || ny<0 || ny>=m) continue;
 			if(a[nx][ny]=='.') continue;
 			q.add(new Point(nx,ny));
 			c[nx][ny]=true;
 			break;
-		}
-		if(q.size()==0){
-			for(int i=0; i<4; i++){
-				int nx = ex + dx[i];
-				int ny = ey + dy[i];
-				if(nx<0 || nx>=n || ny<0 || ny>=m) continue;
-				if(a[nx][ny]=='.') continue;
-				q.add(new Point(nx,ny));
-				c[nx][ny]=true;
-				break;
-			}
 		}
 		String temp = "";
 		while(!q.isEmpty()){
@@ -78,8 +84,10 @@ public class Baek2931 {
 			int x = now.x;
 			int y = now.y;
 			c[x][y]=true;
+			//가스관이 연결되서 갈 수 있는 곳을 확인하기 위해서
 			String pos = p[pipe(a[x][y])];
 			for(int i=0; i<4; i++){
+				//확인할 필요없는 곳은 확인하지않는다.
 				if(pos.charAt(i)=='0') continue;
 				int nx = x + dx[i];
 				int ny = y + dy[i];
@@ -97,15 +105,16 @@ public class Baek2931 {
 				}
 			}
 		}
+		//빈 칸을 찾아냈음 빈칸에서 상하좌우중에 연결해줘야되는 부분을 찾는다
 		for(int i=0; i<4; i++){
 			int nx = ax + dx[i];
 			int ny = ay + dy[i];
-			if(nx<0 || nx>=n || ny<0 || ny>=m || a[nx][ny]=='.') {
+			if(nx<0 || nx>=n || ny<0 || ny>=m) {
 				temp+="0";
 				continue;
 			}
-			if(a[nx][ny]=='Z' || a[nx][ny]=='M'){
-				temp+="1";
+			if(a[nx][ny]=='.' || a[nx][ny]=='Z' || a[nx][ny]=='M'){
+				temp+="0";
 				continue;
 			}
 			temp+=check(ax,ay,nx,ny);
@@ -129,12 +138,9 @@ public class Baek2931 {
 				if(a[i][j]=='M'){
 					sx=i;sy=j;
 				}
-				if(a[i][j]=='Z'){
-					ex=i;ey=i;
-				}
 			}
 		}
-		go();
+		go(new Point(sx,sy));
 		in.close();
 	}
 	public static class Point{
