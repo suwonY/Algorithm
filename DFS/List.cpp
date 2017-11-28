5dfe7c63cd1088e8645ea884fa65c2864f673f75
수원 양 <dongpeeee@gmail.com>
2017-11-28 15:34:51 +0900

    DFS수정

--- DFS/List.cpp
+++ DFS/List.cpp
@@ -1,22 +1,29 @@
-//#include<iostream>
-//using namespace std;
-//int v, e;
-//int a[10][10];
-//bool c[10];
-//void dfs(int start) {
-//	c[start] = true;
-//	cout << start << " ";
-//	
-//	for (int i = 1; i <= v; i++)
-//		if (a[start][i] == 1 && !c[i]) dfs(i);
-//}
-//int main() {
-//	cin >> v >> e;
-//	int from = 0, to = 0;
-//	for (int i = 0; i < e; i++) {
-//		cin >> from >> to;
-//		a[to][from] = a[from][to] = 1;
-//	}
-//	dfs(1);
-//	return 0;
-//}
+#include<iostream>
+#include<vector>
+using namespace std;
+int v, e;
+bool c[5];
+vector<vector<int>> a;
+void dfs(int vertex) {
+	c[vertex] = true;
+	cout << vertex << " ";
+	for (vector<int>::iterator i = a[vertex].begin(); i != a[vertex].end(); i++)
+		if (!c[*i]) dfs(*i);
+}
+int main() {
+	cin >> v >> e;
+	int to = 0, from = 0;
+
+	a.resize(v+1);
+	for (int i = 1; i <= v; i++)
+		a[i].resize(v);
+	
+	for (int i = 0; i < e; i++) {
+		cin >> to >> from;
+		a[to].push_back(from);
+		a[from].push_back(to);
+	}
+	
+	dfs(1);
+	return 0;
+}
